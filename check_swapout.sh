8826b29a6e6a97ce26dd46b586d7d3a8f3728992
Marcos <30581037+marcosvlt@users.noreply.github.com>
2017-11-07 16:36:04 -0200

    Update check_swapout.sh

--- check_swapout.sh
+++ check_swapout.sh
@@ -7,17 +7,14 @@
 SWAPOUT_ACTIVITY=$(vmstat 1 5 | tail -n 4 | awk '{ sum += $8 / 5 } END { print sum }' | awk -F "." '{print $1}')
 SWAP_CRITICAL=1000
 
-echo Swap out activity $SWAPOUT_ACTIVITY
-
 if [ $SWAPOUT_ACTIVITY -lt $SWAP_CRITICAL  ]; then
-    STATUS="0"
-    echo "OK - Swap abaixo de 1/Mbs - $SWAPOUT_ACTIVITY"
+  
+   echo "OK - Swap abaixo de 1/Mbs - $SWAPOUT_ACTIVITY"
 
 elif [ $SWAPOUT_ACTIVITY -ge $SWAP_CRITICAL ]; then
 
-    STATUS="2"
     echo "CRITICAL - Swap acima de 1Mbs - - $SWAPOUT_ACTIVITY"
 
 fi
 
-exit $STATUS
+exit
