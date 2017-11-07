5d164148ee8dd629e20ebfa1a50dcb99d8a4ce3a
Marcos <30581037+marcosvlt@users.noreply.github.com>
2017-11-07 16:24:54 -0200

    Update check_swapout.sh

--- check_swapout.sh
+++ check_swapout.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 # Script to check swap
-# Marcos  10/2017 - V.0.1
+# Marcos  11/2017 - V.0.1
 # ###################################
 
 
 SWAPOUT_ACTIVITY=$(vmstat 1 5 | tail -n 4 | awk '{ sum += $8 / 5 } END { print sum }' | awk -F "." '{print $1}')
-#SWAPOUT_AVERAGE=$( $SWAPOUT_ACTIVITY)
-#SWAP_WARNING=100
-#SWAP_CRITICAL=1000
+SWAP_CRITICAL=1000
 
 echo Swap out activity $SWAPOUT_ACTIVITY
 
-# if [ $SWAPOUT_ACTIVITY -lt $SWAP_WARNING  ]; then
-#       STATUS="0"
-#    echo "OK - Swap abaixo de 100/s - $SWAPOUT_ACTIVITY"
+if [ $SWAPOUT_ACTIVITY -lt $SWAP_CRITICAL  ]; then
+    STATUS="0"
+    echo "OK - Swap abaixo de 1/Mbs - $SWAPOUT_ACTIVITY"
 
-#elif [ $LOAD -ge $PERCENT ] && [ $LOAD -lt $PROCS ]; then
-#    STATUS="1"
-#    echo "WARNING - Status Load Average - $LOAD de $PROCS"
+elif [ $SWAPOUT_ACTIVITY -ge $SWAP_CRITICAL ]; then
 
-#elif [ $LOAD -ge  $PROCS ]; then
-#    STATUS="2"
-#    echo "CRITICAL - Status Load Average - $LOAD de $PROCS"
+    STATUS="2"
+    echo "CRITICAL - Swap acima de 1Mbs - - $SWAPOUT_ACTIVITY"
 
-#fi
+fi
 
-#exit $STATUS
+exit $STATUS
