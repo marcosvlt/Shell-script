4adaaabce3cd209d635d623345dc3e49e53ef049
Marcos <30581037+marcosvlt@users.noreply.github.com>
2017-11-20 11:46:56 -0200

    Update check_load.sh

--- check_load.sh
+++ check_load.sh
@@ -8,11 +8,6 @@ LOAD=`cat /proc/loadavg|awk '{print $1}'|awk -F"." '{print $1}'`
 PERCENT=` awk "BEGIN  {print $PROCS*0.8}" | awk -F "." '{print $1}'  `
 
 
-echo procs $PROCS
-echo load $LOAD
-echo percent $PERCENT
-echo ----------
-
 if [ $LOAD -lt $PERCENT  ]; then
          
         echo "OK - Status Load Average - $LOAD de $PROCS"
