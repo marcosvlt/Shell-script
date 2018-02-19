15c9bddcfb57626a4fcfec22481d984581b03abc
Marcos <30581037+marcosvlt@users.noreply.github.com>
2018-02-19 15:16:59 -0300

    Delete clamavscan.sh

--- clamavscan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCANDIR="/home"
-
-LOG_FILE="/var/log/clamscan_daily.log"
-
-echo "------DATE: `date` ------" >> $LOG_FILE
-clamscan -i -r $SCANDIR >> $LOG_FILE
-echo " ----- END -----" >> $LOG_FILE
