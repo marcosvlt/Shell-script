f3625d1b7cf6bde35ccaadb570d6075299af5b7e
Marcos <30581037+marcosvlt@users.noreply.github.com>
2018-02-19 15:13:46 -0300

    Create clamavscan.sh

---
+++ clamavscan.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCANDIR="/home"
+
+LOG_FILE="/var/log/manual_clamscan.log"
+
+echo "------DATE: `date` ------" >> $LOG_FILE
+clamscan -i -r $SCANDIR >> $LOG_FILE
+echo date >> $LOG_FILE
+echo " ----- END -----" >> $LOG_FILE
