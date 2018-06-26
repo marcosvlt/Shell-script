88a5f5a72b4b09626239b83e4815fc90e2d4a72f
Marcos <30581037+marcosvlt@users.noreply.github.com>
2018-06-26 18:24:03 -0300

    Update check_load.sh

--- check_load.sh
+++ check_load.sh
@@ -3,21 +3,21 @@
 # Marcos  10/2017 - V.0.1
 # ###################################
 
-PROCS=`grep -c ^processor /proc/cpuinfo`
-LOAD=`cat /proc/loadavg|awk '{print $1}'|awk -F"." '{print $1}'`
-PERCENT=` awk "BEGIN  {print $PROCS*0.8}" | awk -F "." '{print $1}'  `
+PROCS=$(grep -c ^processor /proc/cpuinfo)
+LOAD=$(< /proc/loadavg awk '{print $1}' awk -F"." '{print $1}')
+PERCENT=$(awk "BEGIN  {print $PROCS*0.8}" | awk -F "." '{print $1}' )
 
 
-if [ $LOAD -lt $PERCENT  ]; then
+if [ "$LOAD" -lt "$PERCENT"  ]; then
          
         echo "OK - Status Load Average - $LOAD de $PROCS"
 
 
-elif [ $LOAD -ge $PERCENT ] && [ $LOAD -lt $PROCS ]; then
+elif [ "$LOAD" -ge "$PERCENT" ] && [ "$LOAD" -lt "$PROCS" ]; then
        
        echo "WARNING - Status Load Average - $LOAD de $PROCS"
 
-elif [ $LOAD -ge  $PROCS ]; then
+elif [ "$LOAD" -ge  "$PROCS" ]; then
         
         echo "CRITICAL - Status Load Average - $LOAD de $PROCS"
 
