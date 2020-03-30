9928bf531db69747758bf2ec7c2e0e7c29e71bbf
Marcos <30581037+marcosvlt@users.noreply.github.com>
2020-03-30 16:34:29 -0300

    Update aide.sh

--- aide.sh
+++ aide.sh
@@ -13,5 +13,5 @@ mailx -s "$EMAILMSG" -A "$LOGFILE" -A $DB "$EMAILTO" < /dev/null
 
 
 
-# Ideia
+# Ideia to implemente some modification https://wiki.archlinux.org/index.php/AIDE
 # Since the database is stored on the root filesystem, attackers can easily modify it to cover their tracks if they compromise your system. You may want to copy the database to offline, read-only media and perform checks against this copy periodically. 
