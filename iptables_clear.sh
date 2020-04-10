e5f83f4b3555be4c6ce4ae2ea2aead4cf68b4157
Marcos <30581037+marcosvlt@users.noreply.github.com>
2020-04-10 19:11:18 -0300

    Create iptables_clear.sh

---
+++ iptables_clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Change all filter chains to ACCEPT POLICY
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+# Delete all rules in all CHAINS
+iptables -t filter -F
+iptables -t nat -F
+iptables -t mangle -F
+iptables -t raw -F
+
+# Delete user-defined CHAIN
+iptables -X
