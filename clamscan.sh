34ee4b83897fe2ee96f53275c059afbe11230d6e
Marcos <30581037+marcosvlt@users.noreply.github.com>
2020-08-12 15:24:55 -0300

    Update clamscan.sh

--- clamscan.sh
+++ clamscan.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
-LOGFILE="/var/log/clamav/clamav-script-$(date +'%Y-%m-%d_%H%M').log";
-DIRTOSCAN="/";
-INFECTEDDIR="/etc/clamav/infected"
-EMAILMSG="ClamavScan Marcos";
-EMAILTO="mail@mail.com";
+# Autor: Marcos Rosa
+# marcos.rosa@qualitor.com.br
+# 03/2020
+################################
+LOGFILE="/var/log/clamav/clamav-script-$(date +'%Y-%m-%d_%H%M').log"
+DIRTOSCAN="/root"
+EMAILMSG="ClamavScan $(hostname)"
+EMAILTO="marcos.rosa@qualitor.com.br"
 
 echo "
 $(date)
 Starting scan of '$DIRTOSCAN'
 "> $LOGFILE
 
-
-#Create a quarentine folder
-if [ -e $INFECTEDDIR ]; then
-    echo "Infected folder already exist" >> $LOGFILE
-else
-    mkdir $INFECTEDDIR
-    echo "Created infected folder" >> $LOGFILE
-fi
-
-clamscan -ri  --exclude-dir=$INFECTEDDIR --move=$INFECTEDDIR $DIRTOSCAN  >> $LOGFILE
-
-
-#Remove infected files permissions
-if [ "$(ls -A $INFECTEDDIR)" ]; then
-    echo "-------------------------" >> $LOGFILE
-    echo "Removing infected files permissions" >> $LOGFILE
-    chmod -R 000 $INFECTEDDIR
-else
-    echo "Infected folder empty" >> $LOGFILE
-fi
+clamscan -ri $DIRTOSCAN >> $LOGFILE
 
 echo "Complete at $(date)" >> $LOGFILE
 
